67baf27819c65caa58fd3f29bdb9a8fb478546fc
kaushalgosaliya5 <kaushalgosaliya5@gmail.com>
2025-01-31 07:06:19 +0530

    remove favourite folder

--- MVI_Demo_App/app/src/main/java/com/shaileedemo/mvimovies/presentation/favorites/FavoriteUserIntent.kt
+++
@@ -1,9 +0,0 @@
-package com.shaileedemo.mvimovies.presentation.favorites
-
-import com.shaileedemo.mvimovies.domain.ManageFavoriteMovie
-
-class EnterFavoriteSection(private val manageFavoriteMovie: ManageFavoriteMovie) : UserIntent {
-    override suspend fun execute() {
-        manageFavoriteMovie.fetchFavoriteMovies()
-    }
-}
--- MVI_Demo_App/app/src/main/java/com/shaileedemo/mvimovies/presentation/favorites/UserIntent.kt
+++
@@ -1,5 +0,0 @@
-package com.shaileedemo.mvimovies.presentation.favorites
-
-interface UserIntent {
-    suspend fun execute()
-}
